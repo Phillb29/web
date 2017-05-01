5061457e08c71c5f1af9cb1e0c626526244c9ef2
phill baines <p.baines3239@student.leedsbeckettac.uk>
2017-05-01 18:26:35 +0100

    commit opperation

---
+++ commit/SpringBoot2/src/main/java/Greeter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by elder on 01/05/17.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ commit/SpringBoot2/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by elder on 01/05/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
